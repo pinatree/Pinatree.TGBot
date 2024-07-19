b2dcd5a3f52aee0ae027e02577128aa95d465fe1
Alexander Voroshilov <pinatree.workmail@gmail.com>
2024-07-19 10:38:01 +0300

    Добавьте файлы проекта.

---
+++ Pinatree.TGBot/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Pinatree.TGBot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
